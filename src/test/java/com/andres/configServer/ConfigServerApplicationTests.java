91c618ac6687b8a9a59159b6e6947955da19f97f
andresarredondo08 <74028325+andresarredondo08@users.noreply.github.com>
2025-01-29 21:36:50 -0500

    Add files via upload

---
+++ src/test/java/com/andres/configServer/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andres.configServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
